6d9d98018804769470a6cbaa48711dd23994a2e5
shrutilenka <shrutilenka1202@gmail.com>
2022-11-01 19:57:04 +0530

    added untracked file

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily:{
+      body:['proxima-nova','sans-serif'],
+    },
+  },
+  plugins: [],
+}
